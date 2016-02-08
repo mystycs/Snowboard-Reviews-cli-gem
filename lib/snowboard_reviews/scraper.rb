547b3980726956a42dfe3cb96a0e941506af6661
mystycs <mystycs@gmail.com>
2016-02-08 18:13:40 +0000

    final fixes

--- lib/snowboard_reviews/scraper.rb
+++ lib/snowboard_reviews/scraper.rb
@@ -26,32 +26,32 @@ class SnowboardReviews::Scraper
     boards = {}
     boards[:description] = html.css('.reviewfold p').first.text.strip
     if html.css('.reviewfold p').find { |e| e.text =~ /On Snow Feel/ }
-      boards[:onthesnowfeel] = html.css('.reviewfold p').find { |e| e.text =~ /On Snow Feel/ }.text.gsub('On Snow Feel:', '')
+      boards[:onthesnowfeel] = html.css('.reviewfold p').find { |e| e.text =~ /On Snow Feel/ }.text.gsub('On Snow Feel:', '').strip
     else
-       boards[:onthesnowfeel] = []
+      boards[:onthesnowfeel] = []
     end
     if html.css('.reviewfold p').find { |e| e.text =~ /Powder/ }
-      boards[:powder] = html.css('.reviewfold p').find { |e| e.text =~ /Powder/ }.text.gsub('Powder:', '')
+      boards[:powder] = html.css('.reviewfold p').find { |e| e.text =~ /Powder/ }.text.gsub('Powder:', '').strip
     else
       boards[:powder] = []
     end
     if html.css('.reviewfold p').find { |e| e.text =~ /Turn Initiation and Carving/ }
-      boards[:turninitiationandcarving] = html.css('.reviewfold p').find { |e| e.text =~ /Turn Initiation and Carving/ }.text.gsub('Turn Initiation and Carving:', '')
+      boards[:turninitiationandcarving] = html.css('.reviewfold p').find { |e| e.text =~ /Turn Initiation and Carving/ }.text.gsub('Turn Initiation and Carving:', '').strip
     else
       boards[:turninitiationandcarving] = []
     end
     if html.css('.reviewfold p').find { |e| e.text =~ /Speed/ }
-      boards[:speed] = html.css('.reviewfold p').find { |e| e.text =~ /Speed/ }.text.gsub('Speed:', '')
+      boards[:speed] = html.css('.reviewfold p').find { |e| e.text =~ /Speed/ }.text.gsub('Speed:', '').strip
     else
       boards[:speed] = []
     end
     if html.css('.reviewfold p').find { |e| e.text =~ /Flex/ }
-      boards[:flex] = html.css('.reviewfold p').find { |e| e.text =~ /Flex/ }.text.gsub('Flex:', '')
+      boards[:flex] = html.css('.reviewfold p').find { |e| e.text =~ /Flex/ }.text.gsub('Flex:', '').strip
     else
       boards[:flex] = []
     end
     if html.css('.reviewfold p').find { |e| e.text =~ /Jumps/ }
-      boards[:jumps] = html.css('.reviewfold p').find { |e| e.text =~ /Jumps/ }.text.gsub('Jumps:', '')
+      boards[:jumps] = html.css('.reviewfold p').find { |e| e.text =~ /Jumps/ }.text.gsub('Jumps:', '').strip
     else
       boards[:jumps] = []
     end
